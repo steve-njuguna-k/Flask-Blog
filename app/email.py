f36e414c96b02355aefdfcbd048a9e4c51f213f6
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-15 13:34:09 +0300

    Added send email function

---
+++ app/email.py
@@ -0,0 +1,12 @@
+from flask_mail import Message
+from app import mail
+from app import app
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
